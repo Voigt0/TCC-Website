c0480a4eb66b23a69ee97e7449f81b3df39101d5
Larissa <issaschmitz27@gmail.com>
2022-08-05 09:22:50 -0300

    COMMIT 17: Correção no versionamento de arquivos
    Larissa: Correção em erros no versionamento do arquivo

--- view/dispositivo/adicionar-dispositivo.php
+++ view/dispositivo/adicionar-dispositivo.php
@@ -6,25 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Solar Giro</title>
     <link rel="icon" type="image/x-icon" href="../img/favicon/favicon.ico">
-    <link rel="stylesheet" href="../bootstrap-5.2.0-beta1-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../css/addDevice.css">
+    <link rel="stylesheet" href="../../bootstrap-5.2.0-beta1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../css/addDevice.css">
 </head>
 <body>
     <header>
         <nav class="navbar" style="background-color: #171606;">        
             <div class="container-fluid">
-                <div class="nav-element"><a href=""><img src="../img/icons/homeIcon.svg" width="30rem" height="40rem"></a></div>
+                <div class="nav-element"><a href=""><img src="../../img/icons/homeIcon.svg" width="30rem" height="40rem"></a></div>
                 <header>    
-                   <div class="nav-element"><a><img src="../img/icons/solargirologoIconW.svg" style="width: 30vh;"></a></div>
+                   <div class="nav-element"><a><img src="../../img/icons/solargirologoIconW.svg" style="width: 30vh;"></a></div>
                 </header>
-                <div class="nav-element"><a href=""><img src="../img/icons/userIcon.svg" width="40rem"></a></div>
+                <div class="nav-element"><a href=""><img src="../../img/icons/userIcon.svg" width="40rem"></a></div>
             </div>
         </nav>
     </header>
 
     <section>
         <div class="container-fluid">
-            <div class="back"><a href="../index.php"><img src="../img/icons/backIconB.svg" width="60rem"></a></div>
+            <div class="back"><a href="../index.php"><img src="../../img/icons/backIconB.svg" width="60rem"></a></div>
             <div class="container">
                 <div class="box">
                     <div class="form">
@@ -35,22 +35,22 @@
                         </div>
                     <br>
                         <div class="input-field">
-                            <div class="form-element"><label for="name"><img src="../img/icons/nameIcon.svg" width="40rem" height="40rem"></label></div>
+                            <div class="form-element"><label for="name"><img src="../../img/icons/nameIcon.svg" width="40rem" height="40rem"></label></div>
                             <div class="form-element"><input class="" type="" id="name" name="name" placeholder="Nome do dispositivo" value="" required></div>
                         <br>
                         </div>
                         <div class="input-field">
-                            <div class="form-element"><label for="local"><img src="../img/icons/localIcon.svg" width="40rem" height="40rem"></label></div>
+                            <div class="form-element"><label for="local"><img src="../../img/icons/localIcon.svg" width="40rem" height="40rem"></label></div>
                             <div class="form-element"><input class="" type="" id="local" name="local" placeholder="Localização do dispositivo" value="" required></div>
                         <br>
                         </div>
                         <div class="input-field">
-                            <div class="form-element"><label for="datetime"><img src="../img/icons/datetimeIcon.svg" width="40rem" height="40rem"></label></div>
+                            <div class="form-element"><label for="datetime"><img src="../../img/icons/datetimeIcon.svg" width="40rem" height="40rem"></label></div>
                             <div class="form-element"><input class="" type="datetime-local" id="datetime" name="datetime" placeholder="Data e hora" value="" required></div>
                         <br>
                         </div>
                         <div class="input-field">
-                            <div class="form-element"><label for="description"><img src="../img/icons/descriptionIcon.svg" width="40rem" height="40rem"></label></div>
+                            <div class="form-element"><label for="description"><img src="../../img/icons/descriptionIcon.svg" width="40rem" height="40rem"></label></div>
                             <div class="form-element"><input class="description" type="textarea" id="description" name="description" placeholder="Descrição adicional" value=""></div>
                         <br>
                         </div>
--- view/usuario/cadastro.php
+++ view/usuario/cadastro.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Solar Giro</title>
-    <link rel="icon" type="image/x-icon" href="../img/favicon/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="../../img/favicon/favicon.ico">
     <link rel="stylesheet" href="../../bootstrap-5.2.0-beta1-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../../css/register.css">
 </head>
@@ -51,7 +51,7 @@
                         </div>    
                         <br>
                         <div class="input-field">
-                            <div class="form-element"><label for="senha"><img src="./../img/icons/passwordIcon.svg" width="40rem" height="40rem"></label></div>
+                        <div class="form-element"><label for="senha"><img src="../../img/icons/passwordIcon.svg" width="40rem" height="40rem"></label></div>
                             <div class="form-element"><input class="" type="password" id="senha" name="senha" placeholder="Senha" value="" required></div>
                             <br>
                         </div>
